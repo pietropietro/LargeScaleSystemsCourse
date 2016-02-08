dced328d6673d37540f147665c1711f2f8f99a03
pietromini <pietromini@ymail.com>
2016-02-08 11:32:19 +0100

    I changed student calss from eclipse!

--- src/tutorialpointmvc/Student.java
+++ src/tutorialpointmvc/Student.java
@@ -13,7 +13,7 @@ package tutorialpointmvc;
 public class Student {
     private String rollNo;
     private String name;
-   
+    private int phoneNo;
     public String getRollNo() {
         return rollNo;
     }
